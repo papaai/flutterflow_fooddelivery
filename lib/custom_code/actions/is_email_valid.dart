6e55778eea591fe31832fc126dc268f1e55d75a0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-12 12:16:26 +0000

    remove email validator

--- lib/custom_code/actions/is_email_valid.dart
+++ lib/custom_code/actions/is_email_valid.dart
@@ -3,12 +3,11 @@ import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom actions
-import '/flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
 Future<bool> isEmailValid(String emailTxt) async {
   // Add your function code here!
-  retrun(emailTxt.isEmpty ? false : true);
+  return (emailTxt.isEmpty ? false : true);
 }
